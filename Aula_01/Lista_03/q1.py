97b3463f6ff6f72230fff0f1c19e68a06173d6af
Jennifer <jenniferbeatrizmn@gmail.com>
2024-02-22 17:48:59 -0300

    Novas atividades

---
+++ Aula_01/Lista_03/q1.py
@@ -0,0 +1,5 @@
+# ==== Atividade 01 ==== 
+
+print("Informe o seu nome: ")
+nome = str(input())
+print("Welcome to Python,", nome)
---
+++ Aula_01/Lista_03/q2.py
@@ -0,0 +1,10 @@
+# ==== Atividade 01 ==== 
+
+print("Informe o seu nome completo: ")
+
+nome = str(input())
+n = nome.split()
+
+
+print(f"Welcome to Python, {n[0]}")
+#print("Welcome to Python," + n[0])

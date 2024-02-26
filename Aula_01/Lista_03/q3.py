a342811cb253def0b4fa60a93ef4eaa249235d4d
Jennifer <jenniferbeatrizmn@gmail.com>
2024-02-25 21:22:06 -0300

    Nova questão da lista

---
+++ Aula_01/Lista_03/q3.py
@@ -0,0 +1,10 @@
+print("Inserir notas do primeiro e segundo bimestre para calcular sua média parcial.")
+
+print("Insira a nota do primeiro bimestre: ")
+n1 = int(input())
+print("Insira a nota do segundo bimestre: ")
+n2 = int(input())
+
+nf = (n1 + n2) / 2
+
+print("Sua média parcial do semestre equivale a:", nf)
